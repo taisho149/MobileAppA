ca19879f037291d1fef757c4257835f8de2d1161
Taisei Hirano <184594064+taisho149@users.noreply.github.com>
2025-09-04 12:19:32 +0900

    androidロボAA実装

---
+++ app/src/main/java/jp/ac/meijou/android/s241205149/Gist.java
@@ -0,0 +1,10 @@
+package jp.ac.meijou.android.s241205149;
+
+import java.util.Map;
+public class Gist {
+    public Map<String, GistFile> files;
+
+    public static class GistFile {
+        public String content;
+    }
+}
